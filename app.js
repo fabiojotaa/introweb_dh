6bbf2c2aed99cdad1b2909cf643a0e60173d319b
vortex <greghaus001@gmail.com>
2022-05-24 18:11:00 -0300

    estudos web 24-05

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const express = require('express');
+const res = require('express/lib/response');
 const app = express();
+
+
+// app.get('/', (req, res) => {
+//     res.send('Levantando um servidor com Express');
+
+// });
+// app.listen(8000, ()=>
+// console.log('Levantando um servidor com Express')
+// )
+
+
+// app.post('/produto/criar', (req, res) =>res.send(produto));
